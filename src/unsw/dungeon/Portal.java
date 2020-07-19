a041d02f3ff8b7a86368d2ed1799d55b8ffadb40
Ben Charlton <ben.charlton@hotmail.com.au>
2020-07-19 15:39:05 +1000

    portal functionality

--- src/unsw/dungeon/Portal.java
+++ src/unsw/dungeon/Portal.java
@@ -2,43 +2,58 @@ package unsw.dungeon;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+
+import javax.sound.sampled.Port;
 // import unsw.dungeon.Dungeon;
 // import unsw.dungeon.Subject;
 
 public class Portal extends Entity {
 
-    private IntegerProperty exitXCoordinate;
-    private IntegerProperty exitYCoordinate;
+    private int portalPairID;
+    private Dungeon dungeon;
 
-    public Portal(int x, int y) {
+    public Portal(int x, int y, int portalPairID) {
         super(x, y);
-    }
-
-    public void setExitCoordinates(int x, int y) {
-        this.exitXCoordinate = new SimpleIntegerProperty(x);
-        this.exitYCoordinate = new SimpleIntegerProperty(y);
-    }
-
-    public int getExitXCoordinate() {
-        return exitXCoordinate.get();
-    }
-
-    public int getExitYCoordinate() {
-        return exitYCoordinate.get();
+        this.portalPairID = portalPairID;
     }
 
     public void performInteraction(Player player) {
-        // move player to corresponding portal
-        // need to ensure player doesnt move from this after 'interact' is called
-        // fix logic inside player
-        movePlayerToCorrespondingPortal(player);
+        player.x().set(getXCoordToMoveTo(player));
+        player.y().set(getYCoordToMoveTo(player));
     }
 
-    private void movePlayerToCorrespondingPortal(Player player) {
-        int xToMoveTo = getExitXCoordinate();
-        int yToMoveTo = getExitYCoordinate();
-        player.y().set(yToMoveTo);
-        player.x().set(xToMoveTo);
+    private int getXCoordToMoveTo(Player player) {
+        int playerX = player.getX();
+        int fromPortalX = getX();
+        Portal toPortal = getCorrespondingPortal();
+        int toPortalX = toPortal.getX();
+
+        return (playerX - fromPortalX + toPortalX);
+    }
+
+    private int getYCoordToMoveTo(Player player) {
+        int playerY = player.getY();
+        int fromPortalY = getY();
+        Portal toPortal = getCorrespondingPortal();
+        int toPortalY = toPortal.getY();
+
+        return (playerY - fromPortalY + toPortalY);
+    }
+
+
+    private Portal getCorrespondingPortal() {
+        for (Entity entity : dungeon.getEntities()) {
+            if (entity instanceof Portal) {
+                if (((Portal) entity).getPortalPairID() == this.portalPairID){
+                    return (Portal) entity;
+                }
+            }
+        } return null;
+    }
+
+    private int getPortalPairID() {
+        return this.portalPairID;
     }
 
 }
+
