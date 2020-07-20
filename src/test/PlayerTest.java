5f5212e56f8aa51f564cc6439ab3ffb91a1484d8
Mike Gysel <z5251938@ad.unsw.edu.au>
2020-07-20 23:11:01 +1000

    Updated PlayerTest based on coverage checking

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import unsw.dungeon.Dungeon;
 import unsw.dungeon.Player;
+import unsw.dungeon.PlayerStatePattern.Invincible;
+import unsw.dungeon.PlayerStatePattern.PlayerState;
+import unsw.dungeon.PlayerStatePattern.Vulnerable;
 
 /**
  * Test for Player Entity
@@ -16,6 +19,21 @@ public class PlayerTest {
         Player player = new Player(dungeon, 1, 1);
         assertEquals(player.getX(), 1);
         assertEquals(player.getY(), 1);
+    }
+
+    @Test
+    public void correctState() {
+        Dungeon dungeon = new Dungeon(10, 10);
+        Player player = new Player(dungeon, 1, 1);
+        assert(player.getState() instanceof Vulnerable);
+
+        PlayerState invincibleState = new Invincible();
+        player.setPlayerState(invincibleState);
+        assert(player.getState() instanceof Invincible);
+
+        PlayerState vulnerableState = new Vulnerable();
+        player.setPlayerState(vulnerableState);
+        assert(player.getState() instanceof Vulnerable);
     }
 
     @Test
