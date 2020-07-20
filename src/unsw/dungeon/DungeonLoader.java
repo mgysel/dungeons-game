e0e03d658b0c5bbe6e9f7c4e769b169d2c88b77a
Mike Gysel <z5251938@ad.unsw.edu.au>
2020-07-20 17:16:57 +1000

    Updating goals

--- src/unsw/dungeon/DungeonLoader.java
+++ src/unsw/dungeon/DungeonLoader.java
@@ -23,8 +23,7 @@ public abstract class DungeonLoader {
     private JSONObject json;
 
     public DungeonLoader(String filename) throws FileNotFoundException {
-        json = new JSONObject(new JSONTokener(new FileReader("dungeons/boulders.json")));
-        //json = new JSONObject(new JSONTokener(new FileReader("dungeons/" + filename)));
+        json = new JSONObject(new JSONTokener(new FileReader("dungeons/" + filename)));
     }
 
     /**
@@ -86,52 +85,91 @@ public abstract class DungeonLoader {
             break;
         case "wall":
             Wall wall = new Wall(x, y);
+            dungeon.addEntity(wall);
             onLoad(wall);
             entity = wall;
+            break;
+        case "exit":
+            Exit exit = new Exit(dungeon, x, y);
+            dungeon.addEntity(exit);
+            onLoad(exit);
+            entity = exit;
             break;
         case "treasure":
             //if (treasure == goal) {
             // add to goals list
             Treasure treasure = new Treasure(dungeon,x,y);
+            dungeon.addEntity(treasure);
             onLoad(treasure);
             entity = treasure;
-            break;
-        case "boulder":
-            Boulder boulder = new Boulder(dungeon, x, y);
-            onLoad(boulder);
-            entity = boulder;
-            break;
-        case "portal":
-            id = json.getInt("id");
-            Portal portal = new Portal(dungeon, x, y, id);
-            onLoad(portal);
-            entity = portal;
             break;
         case "door":
             id = json.getInt("id");
             Door door = new Door(x,y,id);
+            dungeon.addEntity(door);
             onLoad(door);
             entity = door;
             break;
         case "key":
             id = json.getInt("id");
             Key key = new Key(x,y,id);
+            dungeon.addEntity(key);
             onLoad(key);
             entity = key;
+            break;
+        case "boulder":
+            Boulder boulder = new Boulder(dungeon, x, y);
+            dungeon.addEntity(boulder);
+            onLoad(boulder);
+            entity = boulder;
+            break;
+        case "floor switch":
+            FloorSwitch floorSwitch = new FloorSwitch(dungeon, x, y);
+            dungeon.addEntity(floorSwitch);
+            onLoad(floorSwitch);
+            entity = floorSwitch;
+            break;
+        case "portal":
+            id = json.getInt("id");
+            Portal portal = new Portal(dungeon, x, y, id);
+            dungeon.addEntity(portal);
+            onLoad(portal);
+            entity = portal;
+            break;
+        case "enemy":
+            Enemy enemy = new Enemy(dungeon, x, y);
+            dungeon.addEntity(enemy);
+            onLoad(enemy);
+            entity = enemy;
+            break;
+        case "sword":
+            Sword sword = new Sword(x, y);
+            dungeon.addEntity(sword);
+            onLoad(sword);
+            entity = sword;
+            break;
+        case "invincibility potion":
+            InvincibilityPotion invincibilityPotion = new InvincibilityPotion(x, y);
+            dungeon.addEntity(invincibilityPotion);
+            onLoad(invincibilityPotion);
+            entity = invincibilityPotion;
             break;
         }
         dungeon.addEntity(entity);
     }
 
     public abstract void onLoad(Entity player);
-
     public abstract void onLoad(Wall wall);
-
+    public abstract void onLoad(Exit exit);
     public abstract void onLoad(Treasure treasure);
-
+    public abstract void onLoad(Door door);
+    public abstract void onLoad(Key key);
     public abstract void onLoad(Boulder boulder);
-
+    public abstract void onLoad(FloorSwitch floorSwitch);
     public abstract void onLoad(Portal portal);
+    public abstract void onLoad(Enemy enemy);
+    public abstract void onLoad(Sword sword);
+    public abstract void onLoad(InvincibilityPotion invincibilityPotion);
 
     // TODO Create additional abstract methods for the other entities
     private void addGoalToGame(Object goal) {
